f7ba8677ef467c5b767a2b360dcbfa3c9b435ede
GuyDaniels50 <guydaniels50@gmail.com>
2025-12-01 11:06:09 +1300

    Added excel spreadsheet of team averages, with basic bar chart of win margin

--- data_analysis.R
+++ data_analysis.R
@@ -1,1 +1,16 @@
 library(tidyverse)
+library(readxl)
+library(performance)
+
+team_averages <- read_excel("NZNBL Team averages.xlsx")
+
+team_averages <- team_averages %>%
+  mutate(win_margin = PPG - `Ave Pts Agt`)
+
+team_averages |> 
+  ggplot(aes(x = Team, y = win_margin)) + 
+  geom_col() +
+  geom_text(aes(label = round(win_margin, 1)), vjust = -0.5) +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1)
+    )
