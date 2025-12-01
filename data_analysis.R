3ef89007b2fcc5a1de74ff557dca31c1c53a011b
GuyDaniels50 <guydaniels50@gmail.com>
2025-12-01 18:56:30 +1300

    More detail added to bar chart

--- data_analysis.R
+++ data_analysis.R
@@ -9,11 +9,30 @@ team_averages <- team_averages %>%
 
 team_averages |> 
   ggplot(aes(x = Team, y = win_margin)) + 
-  geom_col() +
-  geom_text(aes(label = round(win_margin, 1)), vjust = -0.5) +
+  geom_col(
+    aes(fill = win_margin >= 0)
+  ) +
+  geom_text(
+    aes(
+    label = round(win_margin, 1),
+    vjust = ifelse(win_margin >= 0, -0.5, 1.2))
+    ) +
+  scale_fill_manual(
+    values = c("FALSE" = "red", "TRUE" = "darkgreen"),
+    guide = "none"
+  ) +
+  scale_y_continuous(limits = c(
+    min(team_averages$win_margin) * 1.2,
+    max(team_averages$win_margin) * 1.2
+  )) +
+  labs(
+    title = "Win Margin by Team",
+    x = "Team",
+    y = "Win Margin"
+  ) +
   theme(
-    axis.text.x = element_text(angle = 45, hjust = 1)
-    )
+  axis.text.x = element_text(angle = 45, hjust = 1)
+  )
 
 margin_proj1 <- lm(win_margin ~ 
                      RPG + APG + STPG + BLKPG + TOPG, data = team_averages)
